3c53400e35a486bd011471159b0c56b355afc021
wanghuiru123 <810462084@qq.com>
2020-04-20 23:29:15 +0800

    用户登录-页面布局-使用element(form)表单组件

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <div class="login-container">登录页面</div>
+  <div class="login-container">
+    <el-form ref="form" :model="user" label-width="80px">
+      <el-form-item label="手机号">
+        <el-input v-model="user.mobile"></el-input>
+      </el-form-item>
+      <el-form-item label="验证码">
+        <el-input v-model="user.code"></el-input>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">登录</el-button>
+      </el-form-item>
+    </el-form>
+  </div>
 </template>
 
 <script>
 export default {
-  name: 'loginIndex'
+  name: 'loginIndex',
+  data () {
+    return {
+      user: {
+        mobile: '',
+        code: ''
+      }
+    }
+  },
+  methods: {
+    onSubmit () {
+      console.log('submit!')
+    }
+  }
 }
 </script>
