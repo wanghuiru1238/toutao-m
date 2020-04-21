a1ccfd4d3e6702b70740a50eefbdd08899d13e16
wanghuiru123 <810462084@qq.com>
2020-04-21 20:29:15 +0800

    用户登录-登录提示信息

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -12,7 +12,7 @@
          <el-checkbox v-model="checked">我已阅读并同意用户协议和隐私条款</el-checkbox>
       </el-form-item>
       <el-form-item>
-        <el-button class="login-btn" type="primary" @click="onlogin">登录</el-button>
+        <el-button :loading="loginLoading" class="login-btn" type="primary" @click="onlogin">登录</el-button>
       </el-form-item>
     </el-form>
   </div>
@@ -28,24 +28,34 @@ export default {
         mobile: '', // 手机号
         code: ''// 验证码
       },
-      checked: false// 是否同意协议选中状态
+      checked: false, // 是否同意协议选中状态
+      loginLoading: false// 登录的loading状态
     }
   },
   methods: {
     onlogin () {
-      // 获取表单数据,表单验证
+      // 获取表单数据
       const user = this.user
-      // 验证通过则登录
+      // 表单验证
+      this.loginLoading = true
       request({
         method: 'POST',
-        url: ' /mp/v1_0/authorizations',
+        url: '/mp/v1_0/authorizations',
+        // 配置post请求体
         data: user
       }).then(res => {
         // 登录成功
+        this.$message({
+          message: '登录成功',
+          type: 'success'
+        })
+        this.loginLoading = false
         console.log(res)
       }).catch(err => {
         // 登录失败
-        console.log(err)
+        console.log('登录失败', err)
+        this.$message.error('登录失败,手机号或验证码错误')
+        this.loginLoading = false
       })
     }
   }
