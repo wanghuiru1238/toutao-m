a4eae7dd3f8301800efb460188a79f689fe1d77e
wanghuiru123 <810462084@qq.com>
2020-04-23 21:42:37 +0800

    首页-layout布局-侧边导航字体图标

--- src/views/layout/components/aside.vue
+++ src/views/layout/components/aside.vue
@@ -10,27 +10,27 @@
       <!-- 此处router作用: 激活导航时 会把index作为path进行路由跳转-->
       <!-- el-menu-item的index属性值不可重复 -->
       <el-menu-item index="/">
-        <i class="el-icon-setting"></i>
+        <i class="el-icon-s-home"></i>
         <span slot="title">首页</span>
       </el-menu-item>
       <el-menu-item index="/article">
-        <i class="el-icon-menu"></i>
+        <i class="el-icon-document"></i>
         <span slot="title">内容管理</span>
       </el-menu-item>
       <el-menu-item index="/image">
-        <i class="el-icon-setting"></i>
+        <i class="el-icon-picture"></i>
         <span slot="title">素材管理</span>
       </el-menu-item>
       <el-menu-item index="/publish">
-        <i class="el-icon-setting"></i>
+        <i class="el-icon-s-promotion"></i>
         <span slot="title">发布文章</span>
       </el-menu-item>
       <el-menu-item index="/comment">
-        <i class="el-icon-setting"></i>
+        <i class="el-icon-chat-dot-round"></i>
         <span slot="title">评论管理</span>
       </el-menu-item>
       <el-menu-item index="/fans">
-        <i class="el-icon-setting"></i>
+        <i class="el-icon-present"></i>
         <span slot="title">粉丝管理</span>
       </el-menu-item>
       <el-menu-item index="/setting">
