8fbf88d239fc3b1a068e5fc79d84d7cd5c798561
wanghuiru123 <810462084@qq.com>
2020-04-23 14:08:04 +0800

    首页-layout布局-侧边导航栏和路由对应

--- src/views/layout/components/aside.vue
+++ src/views/layout/components/aside.vue
@@ -6,33 +6,35 @@
       @close="handleClose"
       background-color="#545c64"
       text-color="#fff"
-      active-text-color="#ffd04b">
+      active-text-color="#ffd04b"
+      router>
+      <!-- 此处router作用: 激活导航时 会把index作为path进行路由跳转-->
       <!-- el-menu-item的index属性值不可重复 -->
-      <el-menu-item index="1">
+      <el-menu-item index="/">
         <i class="el-icon-setting"></i>
         <span slot="title">首页</span>
       </el-menu-item>
-      <el-menu-item index="2">
+      <el-menu-item index="/article">
         <i class="el-icon-menu"></i>
         <span slot="title">内容管理</span>
       </el-menu-item>
-      <el-menu-item index="3">
+      <el-menu-item index="/image">
         <i class="el-icon-setting"></i>
         <span slot="title">素材管理</span>
       </el-menu-item>
-      <el-menu-item index="4">
+      <el-menu-item index="/publish">
         <i class="el-icon-setting"></i>
         <span slot="title">发布文章</span>
       </el-menu-item>
-      <el-menu-item index="5">
+      <el-menu-item index="/comment">
         <i class="el-icon-setting"></i>
         <span slot="title">评论管理</span>
       </el-menu-item>
-      <el-menu-item index="6">
+      <el-menu-item index="/fans">
         <i class="el-icon-setting"></i>
         <span slot="title">粉丝管理</span>
       </el-menu-item>
-      <el-menu-item index="7">
+      <el-menu-item index="/setting">
         <i class="el-icon-setting"></i>
         <span slot="title">个人设置</span>
       </el-menu-item>
