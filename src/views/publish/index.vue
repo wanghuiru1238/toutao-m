1f9f323af0ddf7f067e43dff63c574cfa0da8c66
wanghuiru123 <810462084@qq.com>
2020-05-01 21:00:04 +0800

    发布文章-表单基本数据绑定-文章频道处理

--- src/views/publish/index.vue
+++ src/views/publish/index.vue
@@ -9,25 +9,27 @@
         </el-breadcrumb>
         <!-- 面包屑导航结束 -->
       </div>
-      <el-form ref="form" :model="form" label-width="40px">
+      <el-form ref="form" :model="article" label-width="40px">
         <el-form-item label="标题">
-            <el-input v-model="form.name"></el-input>
+            <el-input v-model="article.title"></el-input>
         </el-form-item>
         <el-form-item label="内容">
-            <el-input type="textarea" v-model="form.desc"></el-input>
+            <el-input type="textarea" v-model="article.content"></el-input>
         </el-form-item>
         <el-form-item label="封面">
-            <el-radio-group v-model="form.resource">
-            <el-radio label="单图"></el-radio>
-            <el-radio label="三图"></el-radio>
-            <el-radio label="无图"></el-radio>
-            <el-radio label="自动"></el-radio>
+            <el-radio-group v-model="article.cover.type">
+            <el-radio :label="1">单图</el-radio>
+            <el-radio :label="3">三图</el-radio>
+            <el-radio :label="0">无图</el-radio>
+            <el-radio :label="-1">自动</el-radio>
             </el-radio-group>
         </el-form-item>
         <el-form-item label="频道">
-            <el-select v-model="form.region" placeholder="请选择频道">
-            <el-option label="区域一" value="shanghai"></el-option>
-            <el-option label="区域二" value="beijing"></el-option>
+            <el-select v-model="article.channel_id" placeholder="请选择频道">
+            <el-option
+            v-for="(channel, index) in channels"
+            :key="index"
+            :label="channel.name" :value="channel.id"></el-option>
             </el-select>
         </el-form-item>
         <el-form-item>
@@ -40,31 +42,40 @@
 </template>
 
 <script>
+import { getArticleChannels } from '@/api/article'
 export default {
   name: 'Publish',
   components: {},
   props: {},
   data () {
     return {
-      form: {
-        name: '',
-        region: '',
-        date1: '',
-        date2: '',
-        delivery: false,
-        type: [],
-        resource: '',
-        desc: ''
+      channels: [], // 文章频道
+      channel_id: null, // 文章频道id
+      article: {
+        title: '', // 文章标题
+        content: '', // 文章内容
+        cover: {
+          type: 0, // 文章类型(-1:自动，0-无图，1-1张，3-3张)
+          images: [] // 文章封面图片地址
+        }
       }
     }
   },
   computed: {},
   watch: {},
-  created () {},
+  created () {
+    this.loadChannels()
+  },
   mounted () {},
   methods: {
     onSubmit () {
       console.log('submit!')
+    },
+    loadChannels () {
+      getArticleChannels().then(res => {
+        // console.log(res)
+        this.channels = res.data.data.channels
+      })
     }
   }
 }
